147ede5b5f2fdf329c6a884d9b1183827c130a46
Alex Alex <mythes@MacBook-Pro.local>
2018-12-06 21:44:18 +0200

    add allure reports

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-public class BasicTest {
+class BasicTest {
     @Tag("localhost")
     @Test
     void firstTest() {
